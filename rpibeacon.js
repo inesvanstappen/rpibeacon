d45f0d01922cfbb1772ac288691c315a119a7ccb
Frederick Bousson <fbousson@gmail.com>
2016-11-15 17:13:57 +0100

    initial commit

---
+++ rpibeacon.js
@@ -0,0 +1,8 @@
+var Bleacon = require('bleacon');
+
+Bleacon.startScanning(); // scan for any bleacons
+
+Bleacon.on('discover', function(bleacon) {
+    // ...
+    console.log("Found a beacon");
+});
