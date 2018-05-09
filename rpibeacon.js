6a7f78ec53dacef40141e73eb8a87c3aef7c2df8
Ines Van Stappen <ines.vanstappen@ordina.be>
2018-05-09 15:48:53 +0200

    add rpi id

--- rpibeacon.js
+++ rpibeacon.js
@@ -6,6 +6,16 @@ Bleacon.on('discover', function (bleacon) {
     // ...
     var currentdate = new Date();
 
+    const exec = require( 'child_process' ).exec;
+    exec('cat /proc/cpuinfo | grep Serial',(error,stdout,stderr) => {
+        if(error){
+            console.error( `exec error: ${error}` );
+            return;
+        }
+        rpiSerial = `stdout: ${stdout}`;// this is your RPI serial number
+        console.log( `stderr: ${stderr}` );
+    });
+
     var datetime = "Last Sync: " + currentdate.getDate() + "/"
         + (currentdate.getMonth() + 1) + "/"
         + currentdate.getFullYear() + " @ "
@@ -13,5 +23,5 @@ Bleacon.on('discover', function (bleacon) {
         + currentdate.getMinutes() + ":"
         + currentdate.getSeconds();
 
-    console.log("Found a beacon\n" + JSON.stringify(bleacon) + '\n'+ datetime);
+    console.log('Found a beacon\n' + rpiSerial + '\n' + JSON.stringify(bleacon) + '\n'+ datetime);
 });
